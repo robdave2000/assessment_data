b5d88a8fb37ee6648f630bf896c6e17a7298281a
Robert Davis <robdave22@yahoo.com>
2021-11-28 22:32:50 -0500

    Step 1 done

--- process.py
+++ process.py
@@ -1,12 +1,18 @@
+#opens file
 log_file = open("um-server-01.txt")
 
-
+#creates a fuction that has a parament of the file opened
 def sales_reports(log_file):
+    #starts a for loop that runs so long as there is a line in the file
     for line in log_file:
+        #sets line equal to line minus \n
         line = line.rstrip()
+        #sets day equal to 3 characters starting at index 0
         day = line[0:3]
-        if day == "Tue":
+        #checks to see if the day is tuesday
+        if day == "Mon":
+            #print the line if the day is tuesday
             print(line)
 
-
+#call the function to run
 sales_reports(log_file)
